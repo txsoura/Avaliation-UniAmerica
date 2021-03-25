66fadc96c286b32b1d8fe71190bee5aa8876df30
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-03-24 22:47:00 -0300

    feat(model): implement vehicle relations

--- src/main/java/com/uniamerica/avaliation/model/Vehicle.java
+++ src/main/java/com/uniamerica/avaliation/model/Vehicle.java
@@ -1,6 +1,7 @@
 package com.uniamerica.avaliation.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="vehicles")
@@ -12,6 +13,9 @@ public class Vehicle {
     private long id;
     private String  registration;
     private int year;
+
+    @OneToMany
+    private List<Brand> brands;
 
     public Vehicle() {
     }
@@ -43,5 +47,13 @@ public class Vehicle {
 
     public void setRegistration(String registration) {
         this.registration = registration;
+    }
+
+    public List<Brand> getBrands() {
+        return brands;
+    }
+
+    public void setModels(List<Brand> brands) {
+        this.brands = brands;
     }
 }
